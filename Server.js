24e6ef5bb44fe3f462f0d57ff1ec9420dcdc56d0
SaghanaEce <107311198+Saghana-saha@users.noreply.github.com>
2025-07-30 23:01:12 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -14,18 +14,14 @@ app.use(express.urlencoded({ extended: true }));
 
 const MONGO_URI = 'mongodb+srv://saghanathangavel:sahana@cluster0.3eoofma.mongodb.net/expense-tracker?retryWrites=true&w=majority&appName=Cluster0';
 
-
 const connectDB = async () => {
-  try {
-    await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log(' Connected to MongoDB');
-  } catch (err) {
-    console.error(' MongoDB connection error:', err);
-    process.exit(1); 
-  }
+try {
+  await mongoose.connect(MONGO_URI);
+  console.log('Connected to MongoDB');
+} catch (err) {
+  console.error('MongoDB connection error:', err);
+  process.exit(1);
+}
 };
 
 
